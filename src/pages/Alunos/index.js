590f32119959f6ff5c981e309df83b3703722847
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-28 13:54:07 -0300

    excluindo alunos

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { get } from "lodash";
 import { Link } from "react-router-dom";
-import { FaUserCircle, FaEdit, FaWindowClose } from "react-icons/fa";
+import {
+  FaUserCircle,
+  FaEdit,
+  FaWindowClose,
+  FaExclamation
+} from "react-icons/fa";
 
 import { Container } from "../../styles/GlobalStyles";
 import axios from "../../services/axios";
 import { AlunoContainer, IconsContainer, ProfilePicture } from "./styled";
 import Loading from "../../components/Loading";
+import { toast } from "react-toastify";
 
 export default function Alunos() {
   const [alunos, setAlunos] = useState([]);
@@ -24,13 +30,46 @@ export default function Alunos() {
     getData();
   }, []);
 
+  const handleDeleteAsk = (e) => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+
+    exclamation.setAttribute("display", "block");
+    e.currentTarget.remove();
+  };
+
+  const handleDelete = async (e, id, index) => {
+    e.persist();
+
+    try {
+      setIsLoading(true);
+      await axios.delete(`/alunos/${id}`);
+      const novosAlunos = [...alunos];
+      novosAlunos.splice(index, 1);
+      setAlunos(novosAlunos);
+      setIsLoading(false);
+    } catch (error) {
+      const status = get(error, "response.status", 0);
+      const errors = get(error, "response.data.errors", []);
+
+      if (status === 401) {
+        toast.error("Você precisa fazer login");
+        setIsLoading(false);
+        return;
+      }
+
+      errors.map((error) => toast.error(error));
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <h1>Alunos</h1>
 
       <AlunoContainer>
-        {alunos.map((aluno) => (
+        {alunos.map((aluno, index) => (
           <div key={aluno.id}>
             <ProfilePicture>
               {get(aluno, "Fotos[0].url", false) ? (
@@ -48,9 +87,16 @@ export default function Alunos() {
                 <FaEdit size={16} />
               </Link>
 
-              <Link to={`/aluno/${aluno.id}/delete`}>
+              <Link onClick={handleDeleteAsk} to={`/aluno/${aluno.id}/delete`}>
                 <FaWindowClose size={16} />
               </Link>
+
+              <FaExclamation
+                size={16}
+                display="none"
+                cursor="pointer"
+                onClick={(e) => handleDelete(e, aluno.id, index)}
+              />
             </IconsContainer>
           </div>
         ))}
